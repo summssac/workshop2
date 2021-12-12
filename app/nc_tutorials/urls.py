fc50fe9666c54d602c3aef6a34960a6b052fdfb7
xx <ccsesrv@gmail.com>
2021-12-11 20:58:26 -0500

    commiting again

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tutorials.urls')),
     path('', include('users.urls'))
-   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
